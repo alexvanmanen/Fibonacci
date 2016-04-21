a405689aa63c8d4eaab4cd63f358ca7f9ae76881
Alex van Manen <alexvanmanen@hotmail.com>
2016-04-21 19:35:23 +0200

    removed duplicated tests in source files

--- src/nl/alexvanmanen/Fibonnaci/FibLoop.java
+++ src/nl/alexvanmanen/Fibonnaci/FibLoop.java
@@ -1,25 +1,6 @@
 package nl.alexvanmanen.Fibonnaci;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 public class FibLoop implements IFibonacci {
-
-	@Test
-	public void test() {
-		int[] array = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
-				10946 };
-		for (int i = 0; i < array.length; i++) {
-			assertEquals(array[i], new FibLoop().fib(i));
-		}
-	}
-
-	@Test(timeout = 1000)
-	public void testPerformance() {
-		assertEquals(12586269025L, fib(50));
-
-	}
-
 	public long fib(int n) {
 		long fib1 = 0, fib2 = 1;
 		long[] array = new long[n + 1];
@@ -31,7 +12,5 @@ public class FibLoop implements IFibonacci {
 		}
 		return array[n];
 	}
-
-
 
 }
